30afe15872f090a7b04af54cb05c74505decded9
hbshih <hbshih@gmail.com>
2020-11-14 14:25:14 +0800

    Initial Commit

---
+++ 句句/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  句句
+//
+//  Created by Ben on 2020/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
