728912ebb2f245ef366ce239baf189a310777c63
hbshih <hbshih@gmail.com>
2020-11-19 00:09:42 +0800

    Share page ui

---
+++ 句句/ShareViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ShareViewController.swift
+//  句句
+//
+//  Created by Ben on 2020/11/18.
+//
+
+import UIKit
+
+class ShareViewController: UIViewController {
+
+    @IBOutlet weak var screenshotPreview: UIImageView!
+    var imageToShow: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+            // screenshotPreview.image = UIImage(named: "icon_notification")
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if imageToShow != nil
+        {
+            screenshotPreview.image = imageToShow
+        }else
+        {
+            screenshotPreview.image = UIImage(named: "icon_notification")
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 句句/ViewController.swift
+++ 句句/ViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundHideenView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "shareSegue"
+        {
+            if let VC = segue.destination as? ShareViewController
+            {
+                backgroundHideenView.isHidden = false
+                let image = takeScreenshot(of: backgroundHideenView)
+                
+                backgroundHideenView.isHidden = true
+                 //   VC.screenshotPreview.image = image
+                
+                VC.imageToShow = image
+                
+              //  VC.screenshotPreview.image = UIImage(named: "icon_notification")
+            }
+        }
+    }
+    
+    // MARK: - Actions
+      @objc func imageWasSaved(_ image: UIImage, error: Error?, context: UnsafeMutableRawPointer) {
+          if let error = error {
+              print(error.localizedDescription)
+              return
+          }
 
+          print("Image was saved in the photo gallery")
+          UIApplication.shared.open(URL(string:"photos-redirect://")!)
+      }
+        
+      func takeScreenshot(of view: UIStackView) -> UIImage {
+          UIGraphicsBeginImageContextWithOptions(
+              CGSize(width: view.bounds.width, height: view.bounds.height),
+              false,
+              2
+          )
+
+          view.layer.render(in: UIGraphicsGetCurrentContext()!)
+          let screenshot = UIGraphicsGetImageFromCurrentImageContext()!
+          UIGraphicsEndImageContext()
+    
+        
+    
+   
+
+    
+    
+       //   UIImageWriteToSavedPhotosAlbum(screenshot, self, #selector(imageWasSaved), nil)
+        return screenshot
+      }
+        
+      @objc func actionButtonTapped() {
+          takeScreenshot(of: backgroundHideenView)
+      }
 
 }
 
